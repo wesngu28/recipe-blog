95144a6abe6fb4357e0c733fb5e5714add920e65
Wesley <serpents123@gmail.com>
2022-12-20 11:19:22 -0800

    larger buttons

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -9,22 +9,22 @@ export default function Categories() {
 
   return(
     <div className="flex mt-1 justify-center flex-wrap">
-      <button onClick={() => filterContext?.setFilter('')} className="m-2 p-1 bg-[beige] hover:bg-[#cac3b7] text-black rounded-lg">
+      <button onClick={() => filterContext?.setFilter('')} className="m-2 p-2 bg-gray-300 hover:bg-[#cac3b7] text-black rounded-lg">
         <p>All</p>
       </button>
-      <button onClick={() => filterContext?.setFilter('Baking')} className="m-2 p-1 bg-[beige] hover:bg-[#cac3b7] text-black rounded-lg">
+      <button onClick={() => filterContext?.setFilter('Baking')} className="m-2 p-2 bg-gray-300 hover:bg-[#cac3b7] text-black rounded-lg">
         <p>Baking</p>
       </button>
-      <button onClick={() => filterContext?.setFilter('Sauces')} className="m-2 p-1 bg-[beige] hover:bg-[#cac3b7] text-black rounded-lg">
+      <button onClick={() => filterContext?.setFilter('Sauces')} className="m-2 p-2 bg-gray-300 hover:bg-[#cac3b7] text-black rounded-lg">
         <p>Sauces</p>
       </button>
-      <button onClick={() => filterContext?.setFilter('Vietnamese')} className="m-2 p-1 bg-[beige] hover:bg-[#cac3b7] text-black rounded-lg">
+      <button onClick={() => filterContext?.setFilter('Vietnamese')} className="m-2 p-2 bg-gray-300 hover:bg-[#cac3b7] text-black rounded-lg">
         <p>Vietnamese</p>
       </button>
-      <button onClick={() => filterContext?.setFilter('Chinese')} className="m-2 p-1 bg-[beige] hover:bg-[#cac3b7] text-black rounded-lg">
+      <button onClick={() => filterContext?.setFilter('Chinese')} className="m-2 p-2 bg-gray-300 hover:bg-[#cac3b7] text-black rounded-lg">
         <p>Chinese</p>
       </button>
-      <button onClick={() => filterContext?.setFilter('Desserts')} className="m-2 p-1 bg-[beige] hover:bg-[#cac3b7] text-black rounded-lg">
+      <button onClick={() => filterContext?.setFilter('Desserts')} className="m-2 p-2 bg-gray-300 hover:bg-[#cac3b7] text-black rounded-lg">
         <p>Desserts</p>
       </button>
     </div>
